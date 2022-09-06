c9b5beecf0e168f6f19719d187c1392039e8f8a6
minhyuck Kim <rhkddka02@naver.com>
2022-09-06 16:13:12 +0900

    C R U D 중에 C만기능구현 O X X X

--- src/main/java/com/springboot/project/controller/Pdcontroller.java
+++ src/main/java/com/springboot/project/controller/Pdcontroller.java
@@ -1,67 +1,111 @@
 package com.springboot.project.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.springboot.project.model.Pdmodel;
-import com.springboot.project.sevice.Pdservice;
-
-import lombok.extern.java.Log;
+import com.springboot.project.model.Pdboard;
+import com.springboot.project.sevice.PdService;
 
 //아래 기본 시큐리티 적용안되게하는구문 시큐리티 구현시 삭제 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @Controller
 public class Pdcontroller {
 
-	@Autowired
-	private Pdservice pdservice;
-	
-	@GetMapping
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
-		return "index";
+//    @RequestMapping("/testBoard")
+//    public String testBoardList(Model model) {
+//        List<Pdboard> pdlist = new ArrayList<Pdboard>();
+//
+//        // 임시로 게시물 10개
+//        for (int i = 0; i < 9; i++) {
+//        	Pdboard pdboard = new Pdboard();
+//        	pdboard.setPdcode(new Long(i));
+//        	pdboard.setTitle("제목   " + i);
+//        	pdboard.setAdmin("작성자 " + i);
+//        	pdboard.setContent("글내용  " + i);
+//        	pdboard.setPrice(new Long(i));
+//        	pdboard.setRegdate(new Date());
+//        	pdlist.add(pdboard);
+//        }
+//        
+//        model.addAttribute("pdlist", pdlist);
+//        return "testBoard"; // jsp 파일 이름
+//    }
+    
+    @Autowired
+    private PdService pdService;
+    
+    @RequestMapping("/getBoardList")
+    public String getpdBoardList(Model model, Pdboard pdboard) {
+        List<Pdboard> pdlist = pdService.getpdBoardList(pdboard);
+        model.addAttribute("pdlist", pdlist);
+        return "getBoardList";
     }
-	
-	  @RequestMapping(value = "/findAll", method = RequestMethod.GET) public
-	  List<Pdmodel> findAll(){ return pdservice.findAll(); }
-	  
-	 @PostMapping("/insert")//글쓰기
-	  public ResponseEntity<?> save(@RequestBody Pdmodel Products){
-		 return new ResponseEntity<>(pdservice.save(Products), HttpStatus.CREATED);
-	  }
-	 
-	 @GetMapping("/board/detail/{pdcode}")//상세보기
-	 public ResponseEntity<?> findById(@PathVariable Long pdcode) {
-		 System.out.println("상세보기실행");
-		 return new ResponseEntity<>(pdservice.select(pdcode),HttpStatus.OK);
-	 }
-	 
-	@DeleteMapping("/delete/{pdcode}")//글삭제
-	public ResponseEntity<?> deleteById(@PathVariable Long pdcode){
-		pdservice.delete(pdcode);
-		return new ResponseEntity<>(pdservice.delete(pdcode),HttpStatus.OK);
-	}
-	
-	@PutMapping("/update/{pdcode}")//글수정
-	public ResponseEntity<?> update(@PathVariable Long pdcode,@RequestBody Pdmodel pdregist){
-		pdservice.update(pdcode, pdregist);
-		return new ResponseEntity<>(pdservice.update(pdcode, pdregist),HttpStatus.OK);
-	}
-	
+    
+    /**
+     * 글쓰기 화면
+     * @return
+     */
+    @RequestMapping("/insertBoardView")
+    public String insertBoardView() {
+        return "pdboard/insertpdBoard";
+    }
+
+    /**
+     * 글쓰기 처리
+     * @param Pdboard
+     * @return
+     */
+    @RequestMapping("/insertBoard")
+    public String insertBoard(Pdboard pdboard) {
+        pdService.insertpdBoard(pdboard);
+        return "redirect:getBoardList";
+    }
+    
+    /**
+            상세 글 보기
+      @param Pdboard
+      @param model
+      @return
+     */
+    @RequestMapping("/getBoard")
+    public String getpdBoard(Pdboard pdboard, Model model) {
+        model.addAttribute("pdboard", pdService.getpdBoard((pdboard)));
+        return "getBoard";
+    }
+    
+    /**
+     * 글 수정 처리 후 목록으로 이동
+     * @param board
+     * @return
+     */
+    
+    @RequestMapping("/updateBoard")
+    public String updateBoard(Pdboard pdboard) {
+        pdService.updatepdBoard(pdboard);
+        return "forward:getBoardList";
+    }
+    
+    /**
+     * 글 삭제 처리 후 목록으로 이동
+     * @param board
+     * @return
+     */
+    
+    @RequestMapping("/deleteBoard")
+    public String deletepdBoard(Pdboard pdboard) {
+        pdService.deletepdBoard(pdboard);
+        return "forward:getBoardList";
+    }
+    
+    
+    
+    
 }
