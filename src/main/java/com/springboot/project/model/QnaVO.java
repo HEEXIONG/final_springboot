0f5b8ecaccf69e7df13d263265d32aa42798164d
qqppoo94 <qqppoo94@naver.com>
2022-09-06 19:26:38 +0900

    qna 리스트 & 글상세

---
+++ src/main/java/com/springboot/project/model/QnaVO.java
@@ -0,0 +1,46 @@
+package com.springboot.project.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "qna_board")
+@EntityListeners(AuditingEntityListener.class)
+public class QnaVO {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "qno")
+	private Long qno;
+	
+	@Column(name = "q_title")
+	private String q_title;
+	
+	@Column(name = "q_content")
+	private String q_content;
+	
+	@Column(name = "q_writer")
+	private String q_writer;
+	
+	@CreatedDate
+	private Date regdate;
+	
+	@CreatedDate
+	private Date updatedate;
+	
+	
+	
+}
