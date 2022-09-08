8353e2dabcd1209543accd1634687b19e46e877a
minhyuck Kim <rhkddka02@naver.com>
2022-09-08 18:10:55 +0900

    9월8일마지막컴파일!

--- src/main/java/com/springboot/project/model/Pdboard.java
+++ src/main/java/com/springboot/project/model/Pdboard.java
@@ -8,8 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.springframework.data.annotation.CreatedDate;
-
 import lombok.Data;
 
 @Data
