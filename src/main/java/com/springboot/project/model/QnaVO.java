41a0178bc4dd789674cff5c52f48cea37eac86a6
minhyuck Kim <rhkddka02@naver.com>
2022-09-13 15:50:52 +0900

    에러...

--- src/main/java/com/springboot/project/model/QnaVO.java
+++ src/main/java/com/springboot/project/model/QnaVO.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.springframework.data.annotation.CreatedDate;
